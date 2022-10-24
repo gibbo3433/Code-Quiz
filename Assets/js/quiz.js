1188210159cdc0c628c7edb2452a20e460aa3d0d
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2022-10-24 14:33:16 +0100

    added Var for quiz.js

---
+++ Assets/js/quiz.js
@@ -0,0 +1,15 @@
+var startQuiz = document.getElementById("startQuiz")
+var saveScore = document.getElementById("saveScore")
+var playAgain = document.getElementById("playAgain")
+
+var welcome = document.getElementById("welcome")
+
+var quiz = document.getElementById("quiz")
+var question = document.getElementById("question")
+var options = document.getElementById("options")
+var message = document.getElementById("message")
+
+var result = document.getElementById("result")
+var summary = document.getElementById("summary")
+var scores = document.getElementById("scores")
+
