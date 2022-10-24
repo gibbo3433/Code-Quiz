87ee347de7b71e4a05e2db262839cadc6f2b0bc8
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2022-10-24 17:17:41 +0100

    added the stop game and save score function

--- Assets/js/quiz.js
+++ Assets/js/quiz.js
@@ -12,4 +12,35 @@ var message = document.getElementById("message")
 var result = document.getElementById("result")
 var summary = document.getElementById("summary")
 var scores = document.getElementById("scores")
+var timer = document.getElementById("timer")
+var initials = document.getElementById("initials")
+
+var secondsleft = 0;
+var score = 0;
+var currentQuestion = 0;
+var countdownTimer;
+
+function stopGame() {
+    
+    // This will stop the countdown timer when the game stops
+    clearInterval(countdownTimer)
+
+    // This will clear the text context of the timer
+    timer.textContent = "";
+
+    // This line will hide the quiz style, and display the result style
+    quiz.style.display = "none";
+    result.style.display = "flex";
+
+    // This will deisplay the score of the most recent game
+    summary.textContent.display = " Your score this game was " + score;
+}
+
+// This function will save the recent initals and  score to the local storage 
+function SaveScore () {
+    if  (initials !== "" ) {
+        localStorage.setItem(initials, score);
+        document.getElementById("initals").value = "";
+    }
+}
 
